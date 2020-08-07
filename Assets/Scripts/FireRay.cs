278801682038180e78bd3d1e1294aaef80d996dc
pat <patrickfungdesigns@gmail.com>
2020-08-07 17:04:44 +1000

    start scene sound added, particle for debris, rocks falling

--- Assets/Scripts/FireRay.cs
+++ Assets/Scripts/FireRay.cs
@@ -6,7 +6,15 @@ public class FireRay : MonoBehaviour
 {
     RaycastHit rayhit;
     Ray ray;
-    float distanceRay = 20; 
+    float distanceRay = 20;
+
+    public AudioSource earthQuake;
+    public AudioSource smallrockSlide;
+    public AudioSource largeRockSlide;
+
+    public GameObject fallingStones;
+    public GameObject fallingStones2; 
+
 
 
 
@@ -22,7 +30,29 @@ public class FireRay : MonoBehaviour
                 {
                     Destroy(rayhit.collider.gameObject); 
                 }
+
+                if (rayhit.collider.gameObject.name == "Artifact")
+                {
+
+                    //delete that artifact
+                    Destroy(rayhit.collider.gameObject); 
+                    //play sound of earthquake
+                    earthQuake.Play();
+                    smallrockSlide.Play();
+                    fallingStones.SetActive(true); 
+                    StartCoroutine(nextSound());
+                    //floor need to open and player falls
+                }
             }
+        }
+
+        IEnumerator nextSound()
+        {
+            yield return new WaitForSeconds(5);
+            largeRockSlide.Play(); 
+            fallingStones2.SetActive(true); 
+            //the floor gates to opena
+            
         }
 
         Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * distanceRay); 
